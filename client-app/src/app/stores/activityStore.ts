aa469f9f510e517c38a46f515b91de4a73cd02d5
Jay Raja <jayraja15@hotmail.com>
2020-04-27 17:42:50 +0100

    End of section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -13,8 +13,21 @@ class ActivityStore {
   @observable target = "";
 
   @computed get activitiesByDate() {
-    return Array.from(this.activityRegistry.values()).sort(
+    return this.groupActivitiesByDate(
+      Array.from(this.activityRegistry.values())
+    );
+  }
+
+  groupActivitiesByDate(activities: IActivity[]) {
+    const sortedActivities = activities.sort(
       (a, b) => Date.parse(a.date) - Date.parse(b.date)
+    );
+
+    return Object.entries(sortedActivities.reduce((activities, activity) => {
+        const date = activity.date.split('T')[0];
+        activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+        return activities;
+      }, {} as { [key: string]: IActivity[]})
     );
   }
 
@@ -42,18 +55,17 @@ class ActivityStore {
     let activity = this.getActivity(id);
     if (activity) {
       this.activity = activity;
-    }
-    else{
+    } else {
       try {
         activity = await agent.Activities.details(id);
-        runInAction('getting activity', ()=> {
+        runInAction("getting activity", () => {
           this.activity = activity;
           this.loadingInitial = false;
-        })
+        });
       } catch (error) {
-        runInAction('getting activity error',()=> {
-         this.loadingInitial = false; 
-        })
+        runInAction("getting activity error", () => {
+          this.loadingInitial = false;
+        });
         console.log(error);
       }
     }
@@ -61,7 +73,7 @@ class ActivityStore {
 
   @action clearActivity = () => {
     this.activity = null;
-  }
+  };
 
   getActivity = (id: string) => {
     return this.activityRegistry.get(id);
@@ -121,7 +133,6 @@ class ActivityStore {
       console.log(error);
     }
   };
-
 }
 
 export default createContext(new ActivityStore());
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,16 +1,16 @@
 import React, { useState, FormEvent, useContext, useEffect } from 'react';
-import { Segment, Form, Button } from 'semantic-ui-react';
+import { Segment, Form, Button, Grid } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import { v4 as uuid } from 'uuid';
 import ActivityStore from '../../../app/stores/activityStore';
 import { observer } from 'mobx-react-lite';
 import { RouteComponentProps } from 'react-router-dom';
 
-interface DetailsParams{
-    id:string;
+interface DetailsParams {
+    id: string;
 }
 
-const ActivityForm: React.FC<RouteComponentProps<DetailsParams>> = ({match, history}) => {
+const ActivityForm: React.FC<RouteComponentProps<DetailsParams>> = ({ match, history }) => {
     const activityStore = useContext(ActivityStore);
     const {
         createActivity,
@@ -32,8 +32,8 @@ const ActivityForm: React.FC<RouteComponentProps<DetailsParams>> = ({match, hist
 
 
     useEffect(() => {
-        if(match.params.id && activity.id.length === 0){
-            loadActivity(match.params.id).then(() =>  initialFormState && setActivity(initialFormState));
+        if (match.params.id && activity.id.length === 0) {
+            loadActivity(match.params.id).then(() => initialFormState && setActivity(initialFormState));
         }
 
         return () => {
@@ -61,23 +61,31 @@ const ActivityForm: React.FC<RouteComponentProps<DetailsParams>> = ({match, hist
     }
 
     return (
-        <Segment clearing>
-            <Form onSubmit={handleSubmit}>
-                <Form.Input onChange={handleInputChange} name='title' placeholder='Title' value={activity.title} />
-                <Form.TextArea onChange={handleInputChange} name='description' rows={2} placeholder='Description' value={activity.description} />
-                <Form.Input onChange={handleInputChange} name='category' placeholder='Category' value={activity.category} />
-                <Form.Input onChange={handleInputChange} name='date' type='datetime-local' placeholder='Date' value={activity.date} />
-                <Form.Input onChange={handleInputChange} name='city' placeholder='City' value={activity.city} />
-                <Form.Input onChange={handleInputChange} name='venue' placeholder='Venue' value={activity.venue} />
-                <Button loading={activityStore.submitting} floated='right' positive type='submit' content='Submit' />
-                <Button
-                    onClick={() => history.push('/activities')}
-                    floated='right'
-                    type='button'
-                    content='Cancel'
-                />
-            </Form>
-        </Segment>
+        <Grid>
+            <Grid.Column width={10}>
+                <Segment clearing>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Input onChange={handleInputChange} name='title' placeholder='Title' value={activity.title} />
+                        <Form.TextArea onChange={handleInputChange} name='description' rows={2} placeholder='Description' value={activity.description} />
+                        <Form.Input onChange={handleInputChange} name='category' placeholder='Category' value={activity.category} />
+                        <Form.Input onChange={handleInputChange} name='date' type='datetime-local' placeholder='Date' value={activity.date} />
+                        <Form.Input onChange={handleInputChange} name='city' placeholder='City' value={activity.city} />
+                        <Form.Input onChange={handleInputChange} name='venue' placeholder='Venue' value={activity.venue} />
+                        <Button loading={activityStore.submitting} floated='right' positive type='submit' content='Submit' />
+                        <Button
+                            onClick={() => history.push('/activities')}
+                            floated='right'
+                            type='button'
+                            content='Cancel'
+                        />
+                    </Form>
+                </Segment>
+
+            </Grid.Column>
+
+        </Grid>
+
+
     )
 }
 
